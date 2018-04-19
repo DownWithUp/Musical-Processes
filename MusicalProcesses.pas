53bcc2ac36837ea6c4489c855bcd2d38ac81bbe0
StealthyByte <16905064+StealthyByte@users.noreply.github.com>
2018-04-18 20:46:02 -0400

    Update MusicalProcesses.pas

--- MusicalProcesses.pas
+++ MusicalProcesses.pas
@@ -1,21 +1,13 @@
 program MusicalProcesses;
-//For entertainment purposes only
+//For entertainment purposes only :)
 
 {$APPTYPE CONSOLE}
 
 uses
-  SysUtils,Windows;
-
-
-type
-  THMODULE = ^HMODULE;
-
+  SysUtils, Windows, PSAPI;
 
 function QueryFullProcessImageNameA(hProcess : THANDLE; dwFlags : DWORD; lpExeName : LPSTR;
                                  lpdwSize : PDWORD) : DWORD; stdcall; external 'kernel32.dll' name 'QueryFullProcessImageNameA';
-
-function EnumProcessModules(hProcess : THANDLE; lphModule : THMODULE; cb : DWORD;
-                            lpcbNeeded : LPDWORD) : DWORD; stdcall; external 'PSAPI.dll'
 
 function isOnlyNumbers(szInput : String) : Boolean;
 var
@@ -36,31 +28,96 @@ end;
 var
 hProcess : THandle;
 nPID : Integer;
-pImageFileName : PChar;
 value : DWORD = MAX_PATH;
-lpExeNames: array[0..255] of char;
+szExeName: array[0..1024] of char;
+cbNeeded, dwBytesRead  : DWORD;
+phMods, phMod : PDWORD;
+nTemp, nTemp2, i : integer;
+szModName : array [0..1024] of Char;
+ModInfo : MODULEINFO;
+lpBuffer : array [0..1024] of Byte;
 
+procedure printHelp();
+begin
+  writeln('Usage: ' + ExtractFileName(GetCurrentDir) + '.exe [PID] [BAA] [SPEED]' );
+  writeln(#9 + 'PID = Process Id (32 bit only)');
+  writeln(#9 + 'NoB = Number of Bytes (to play) (MIN 2 = 2 Bytes, 4 = 4 Bytes MAX)');
+  writeln(#9 + 'SPEED = The duration of each note (MIN 200, 4000 MAX)');
+end;
+
+procedure processBeeps(dwDIL, dwSpeed : DWORD);
+var
+i : integer;
+wTemp : Word;
+begin
+  for i := 0 to sizeOf(lpBuffer) do begin
+    case dwDIL of
+    2: wTemp := lpBuffer[i];
+    4: wTemp := lpBuffer[i] * $100 + lpBuffer[i+1];
+    end;
+    Writeln('[i] Note: 0x' + IntToHex(wTemp, 2));
+    Beep(wTemp, dwSpeed);
+  end;
+  Writeln('[i] Thank you for listening!');
+  Halt(0); //End of music, so end the process
+end;
 
 begin
-  if (paramCount <> 1) then begin
-    writeln('Usage: ' + ExtractFileName(GetCurrentDir) + '.exe [PID]' );
+  if (paramCount <> 3) then begin
+    printHelp;
     Halt(0);
   end else begin //Try to extract PID
-    if (isOnlyNumbers(ParamStr(1))) then begin
-      nPid := StrToInt(ParamStr(1));
-      hProcess := OpenProcess(PROCESS_ALL_ACCESS, false, nPID);
-      QueryFullProcessImageNameA(hProcess, 0, lpExeNames, @value);
-      writeln(lpExeNames);
-      EnumProcessModules(hProcess,
-
-      readln;
-      Halt(0);
-
+    if (isOnlyNumbers(ParamStr(1)) and isOnlyNumbers(ParamStr(2)) and isOnlyNumbers(ParamStr(2))) then begin
+      nTemp := StrToInt(ParamStr(2));
+      if (nTemp in [2, 4]) then begin
+        nTemp := StrToInt(ParamStr(3));
+        if (nTemp <= 4000) and (nTemp >= 200) then begin
+          nPid := StrToInt(ParamStr(1));
+          hProcess := OpenProcess(PROCESS_ALL_ACCESS, false, nPID);
+          if (hProcess = 0) then begin //OpenProcess failed
+            Writeln('[!] Error: OpenProcess failed!');
+            Halt(0);
+          end;
+          QueryFullProcessImageNameA(hProcess, 0, szExeName, @value);
+          writeln('[i] Targeting: ', szExeName);
+          phMods := nil; //Not needed for first call to EnumProcessModules
+          EnumProcessModules(hProcess, phMods, 0, cbNeeded); //See how many modules we needs
+          GetMem(phMods, cbNeeded); //hMods now has enough room
+          EnumProcessModules(hProcess, phMods, cbNeeded, cbNeeded);
+          phMod := phMods;
+          for i := 0 to (cbNeeded div sizeOf(THANDLE)) -1 do begin
+            GetModuleFileNameEx(hProcess, phMod^, @szModName, sizeOf(szModName));
+            //write(szModName, ' | ', IntToStr(phMod^)); //Debug only, NewLine included
+            if(trim(szModName) = trim(szExeName)) then begin
+              //Found the right module
+              GetModuleInformation(hProcess, phMod^, @ModInfo, sizeOf(ModInfo));
+              Writeln('[i] Entry Point: 0x', IntToHex(Integer(ModInfo.EntryPoint), 8));
+              Writeln('[i] Estimated Size Of Image (Length of Music): 0x', IntToHex(Integer(ModInfo.SizeOfImage), 8));
+              ReadProcessMemory(hProcess, ModInfo.EntryPoint, @lpBuffer, sizeOf(lpBuffer), dwBytesRead);
+              Writeln('Found start of code. Press [RETURN] to play...');
+              Readln;
+              nTemp := StrToInt(ParamStr(2)); nTemp2 := StrToInt(ParamStr(3));
+              processBeeps(nTemp, nTemp2); //Play the music
+          end;
+          Inc(phMod);
+          Readln;
+        end;
+        readln;
+        Halt(0);
+      end else begin
+        writeln('[!] Error: Invalid SPEED');
+        printHelp;
+        Halt(0);
+      end;
     end else begin
-      writeln('[!] Error: invalid PID');
+      writeln('[!] Error: Invalid NoB');
       Halt(0);
     end;
+  end else begin
+    writeln('[!] Error: Invalid Parameters');
+    printHelp;
+    Halt(0);
   end;
-
+  end;
   Readln;
 end.
